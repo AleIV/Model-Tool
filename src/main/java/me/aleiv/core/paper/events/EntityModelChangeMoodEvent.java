e3a88608773735ba7e876eca68c4b0469bd9d564
DolphinGamerYT <dolphingamerytof@gmail.com>
2021-12-29 08:45:48 -0800

    Made EntityModelChangeMoodEvent

--- src/main/java/me/aleiv/core/paper/events/EntityModelChangeMoodEvent.java
+++ src/main/java/me/aleiv/core/paper/events/EntityModelChangeMoodEvent.java
@@ -1,7 +1,18 @@
 package me.aleiv.core.paper.events;
 
-public class EntityModelChangeMoodEvent {
+import lombok.Getter;
+import me.aleiv.core.paper.core.EntityModel;
+import me.aleiv.core.paper.models.EntityMood;
 
-    // TODO
-    
+public class EntityModelChangeMoodEvent extends EntityModelEvent {
+
+    @Getter private final EntityMood oldMood;
+    @Getter private final EntityMood newMood;
+
+    public EntityModelChangeMoodEvent(EntityModel entityModel, EntityMood oldMood, EntityMood newMood) {
+        super(entityModel);
+        this.oldMood = oldMood;
+        this.newMood = newMood;
+    }
+
 }
