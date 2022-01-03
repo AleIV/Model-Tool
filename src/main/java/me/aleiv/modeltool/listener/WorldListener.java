a6dbab158990619240bcfcf13c44a5a5c7a41174
DolphinGamerYT <dolphingamerytof@gmail.com>
2022-01-02 17:22:07 -0800

    Fixed restoring entities

--- src/main/java/me/aleiv/modeltool/listener/WorldListener.java
+++ src/main/java/me/aleiv/modeltool/listener/WorldListener.java
@@ -4,24 +4,23 @@ import com.ticxo.modelengine.api.ModelEngineAPI;
 import me.aleiv.modeltool.core.EntityModelManager;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.server.PluginDisableEvent;
-import org.bukkit.event.world.WorldLoadEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.WorldUnloadEvent;
 
+import java.util.List;
 import java.util.Objects;
 
 public class WorldListener implements Listener {
 
     private final EntityModelManager manager;
-    private boolean hasPlayerJoined;
 
     public WorldListener(EntityModelManager manager) {
         this.manager = manager;
-        this.hasPlayerJoined = false;
     }
 
     @EventHandler(priority = EventPriority.LOW)
@@ -31,27 +30,18 @@ public class WorldListener implements Listener {
         this.unloadWorld(e.getWorld());
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onDisable(PluginDisableEvent e) {
         Bukkit.getWorlds().forEach(this::unloadWorld);
     }
 
-    @EventHandler(priority = EventPriority.HIGH)
-    public void onWorldLoad(WorldLoadEvent e) {
-        if (this.hasPlayerJoined) {
-            this.checkWorld(e.getWorld());
-        }
+    @EventHandler
+    public void onChunkLoad(ChunkLoadEvent e) {
+        this.checkEntities(List.of(e.getChunk().getEntities()));
     }
 
-    @EventHandler(priority = EventPriority.LOW)
-    public void onPlayerJoin(PlayerJoinEvent e) {
-        if (this.hasPlayerJoined) return;
-        this.hasPlayerJoined = true;
-        Bukkit.getWorlds().forEach(this::checkWorld);
-    }
-
-    public void checkWorld(World world) {
-        world.getEntities().stream().filter(m -> ModelEngineAPI.api.getModelManager().getModeledEntity(m.getUniqueId()) != null).forEach(entity -> Bukkit.getScheduler().runTask(manager.getJavaPlugin(), () -> manager.restoreEntityModel(entity)));
+    public void checkEntities(List<Entity> entities) {
+        entities.stream().filter(m -> ModelEngineAPI.api.getModelManager().getModeledEntity(m.getUniqueId()) != null).forEach(entity -> Bukkit.getScheduler().runTask(manager.getJavaPlugin(), () -> manager.restoreEntityModel(entity)));
     }
 
     public void unloadWorld(World world) {
